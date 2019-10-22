f0550163b491ecacd0f3183f0eff2cc9db16d7f3
4mokie <dla.gazet@gmail.com>
2019-10-22 17:08:02 -0400

    exp Isw( cos )

--- JJformulas.py
+++ JJformulas.py
@@ -147,3 +147,121 @@ def  II( R1, R2):
 def  Rj( RN, T ):
     α = 1e1
     return α*RN*np.exp( -1/Δ*T*kB )
+
+#####################################################
+def avg_group(vA0, vB0):
+    vA0 = np.round(vA0*1e15)/1e15   #remove small deferences
+    vB0 = np.round(vB0*1e15)/1e15
+    
+    vA, ind, counts = np.unique(vA0, return_index=True, return_counts=True) # get unique values in vA0
+    vB = vB0[ind]
+    for dup in vB[counts>1]: # store the average (one may change as wished) of original elements in vA0 reference by the unique elements in vB
+        vB[np.where(vA==dup)] = np.average(vB0[np.where(vA0==dup)])
+    return vA, vB
+
+
+def cut_dxdy(vA0, vB0, dx,dy):
+    
+    ind1 = np.where(np.abs(vA0) < dx )
+    vA1, vB1 = vA0[ind1], vB0[ind1]
+
+    ind2 = np.where(np.abs(vB1) < dy )
+    vA, vB = vA1[ind2], vB1[ind2]
+
+    return vA, vB
+
+
+def V_func(I,V, val):
+    out = []
+    for x in np.nditer(val):
+        out = np.append (out,  V[np.argmin(abs(I-x))])
+    return out
+
+
+def diffArr(Xarr, Yarr, step):
+    out = []
+    for x in Xarr:
+        out = np.append(out, np.mean((V_func(Xarr,Yarr, x+step/2))  - np.mean(V_func(Xarr,Yarr, x-step/2)))/(step))
+    return out
+
+
+def R0byFit (I,V,n = 3):
+    V = np.append(V, V[-n:])
+    I = np.append(I, I[-n:])
+    
+    out = []
+    
+    for i in range(len(I)-n):    
+        a, b = polyfit (I [i:i+n] , V [i:i+n], 1 )
+        out = np.append(out, a)
+        
+    return out
+
+def XYEqSp(Xarr, Yarr, step):
+    outX = []
+    outY = []
+
+    n = int((np.max(Xarr) - np.min(Xarr)) // step)    
+    
+    for i in range(n):
+        outX = np.append( outX, V_func(Xarr, Xarr, np.min(Xarr) + i*step)  )
+        outY = np.append( outY, V_func(Xarr, Yarr, np.min(Xarr) + i*step)  )
+
+    return outX, outY
+
+
+def offsetRemove(I,V, Istep, mode = 'ZF', Ioff_def = 8e-12):
+       
+    Rdiff = Rdiff_TVReg(V, Istep)
+    
+    ind_minR = np.argmin(Rdiff)
+    ind_maxR = np.argmax(Rdiff)
+
+
+    if mode == 'ZF':
+        Ioff = I[ind_minR]
+    else:
+        Ioff = Ioff_def
+  
+    Voff = V_func(I, V, Ioff)
+
+    Inew = I - Ioff
+    Vnew = V - Voff
+   
+
+    return Inew, Vnew
+
+# def Rdiff_TVReg(V, Istep):
+#     stepx = 0.05
+#     Rdiff = (TVRegDiff(V, 100, 10e-3, dx = stepx, ep=1e-1, scale='small', plotflag=0)*stepx / Istep)[:-1]
+#     return Rdiff
+
+def eng_string( x, sig_figs=3, si=True):
+    x = float(x)
+    sign = ''
+    if x < 0:
+        x = -x
+        sign = '-'
+    if x == 0:
+        exp = 0
+        exp3 = 0
+        x3 = 0
+    else:
+        exp = int(math.floor(math.log10( x )))
+        exp3 = exp - ( exp % 3)
+        x3 = x / ( 10 ** exp3)
+        x3 = round( x3, -int( math.floor(math.log10( x3 )) - (sig_figs-1)) )
+        if x3 == int(x3): # prevent from displaying .0
+            x3 = int(x3)
+
+    if si and exp3 >= -24 and exp3 <= 24 and exp3 != 0:
+        exp3_text = 'yzafpnum kMGTPEZY'[ exp3 // 3 + 8]
+    elif exp3 == 0:
+        exp3_text = ''
+    else:
+        exp3_text = 'e%s' % exp3
+
+    return ( '%s%s%s') % ( sign, x3, exp3_text)
+
+
+
